3805fb144586782fe9358c6bae2e0833758f1bc9
Gerardo <gerodel98@gmail.com>
2019-12-08 10:14:25 -0600

    Created basic structure for Ask4Help view

---
+++ grails-app/views/ask4Help/index.gsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="layout" content="main" />
+        <title><g:message code="default.list.label" args="[entityName]" /></title>
+    </head>
+    <body>
+
+    </body>
+</html>
---
+++ src/test/groovy/sistema_experto/Ask4HelpControllerSpec.groovy
@@ -0,0 +1,18 @@
+package sistema_experto
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class Ask4HelpControllerSpec extends Specification implements ControllerUnitTest<Ask4HelpController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
