bcbb5eafdb236b760de3b22ae5d55b4dd100cea4
Gerardo <gerodel98@gmail.com>
2019-12-08 22:23:49 -0600

    Created content for Statement domain and initialized

--- grails-app/domain/sistema_experto/Statement.groovy
+++ grails-app/domain/sistema_experto/Statement.groovy
@@ -1,7 +1,15 @@
 package sistema_experto
 
 class Statement {
-
+    String literal
+    String descripcion
+    Tipo tipo
     static constraints = {
     }
+}
+
+enum Tipo {
+  ANTECEDENTE,
+  CONSECUENTE,
+  AMBOS
 }
